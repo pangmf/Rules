8cdd4c66fe5d5f7d135aa81925d7071964df2771
pangmf <41246258+pangmf@users.noreply.github.com>
2026-09-23 11:07:33 +0800

    Update script path for zeekr.js

--- Shadowrocket/task/zeekr.js
+++ Shadowrocket/task/zeekr.js
@@ -2,9 +2,9 @@
 #!desc= 获取Token方法 : 进入APP获得
 
 [Script]
-zeekr = type=http-response, pattern=^https:\/\/api-gw-toc\.zeekrlife\.com\/zeekrlife-app-user\/v\d\/user\/info\/query$, script-path=https://raw.githubusercontent.com/wf021325/qx/master/task/zeekr.js, requires-body=true, max-size=-1, timeout=60
+zeekr = type=http-response, pattern=^https:\/\/api-gw-toc\.zeekrlife\.com\/zeekrlife-app-user\/v\d\/user\/info\/query$, script-path=https://raw.githubusercontent.com/pangmf/Rules/refs/heads/master/Shadowrocket/task/zeekr.js, requires-body=true, max-size=-1, timeout=60
 
-极氪汽车签到 = type=cron, cronexp=1 0 * * *, script-path=https://raw.githubusercontent.com/wf021325/qx/master/task/zeekr.js, timeout=120, wake-system=1
+极氪汽车签到 = type=cron, cronexp=1 0 * * *, script-path=https://raw.githubusercontent.com/pangmf/Rules/refs/heads/master/Shadowrocket/task/zeekr.js, timeout=120, wake-system=1
 
 [MITM]
 hostname = %APPEND% api-gw-toc.zeekrlife.com
